2247e94a487445f7d252868d7a71970e17f0dbaf
Oskar Rough <oskar@rough.dk>
2018-07-09 18:42:32 +0200

    Lint

--- app/routes/quiz/questions.js
+++ app/routes/quiz/questions.js
@@ -1,8 +1,5 @@
 import Route from '@ember/routing/route'
-import {hash} from 'rsvp'
 import {pickRandom} from 'trivia-radio4000/utils/random-helpers'
-
-const AMOUNT_QUESTIONS = 5
 
 export default Route.extend({
   async model() {
