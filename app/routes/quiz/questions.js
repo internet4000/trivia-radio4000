85a0d54e51d181e12050144445d27ee37e84c420
Oskar Rough <oskar@rough.dk>
2018-07-09 21:55:39 +0200

    Better

--- app/routes/quiz/questions.js
+++ app/routes/quiz/questions.js
@@ -2,13 +2,10 @@ import Route from '@ember/routing/route'
 import {pickRandom} from 'trivia-radio4000/utils/random-helpers'
 
 export default Route.extend({
-  beforeModel() {
-    let quiz = this.modelFor('quiz')
-    if (!quiz.get('channel.id')) this.transitionTo('quiz.index')
-  },
   async model() {
     let quiz = this.modelFor('quiz')
     let tracks = await quiz.get('channel.tracks')
+    if (!tracks) this.transitionTo('quiz.index')
     let questions = pickRandom(tracks, quiz.get('questionNumber'))
     return questions
   }
