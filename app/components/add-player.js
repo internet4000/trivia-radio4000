8ab9e1dc67491e9c7a832be688b62d572e92c616
Oskar Rough <oskar@rough.dk>
2018-07-09 15:30:38 +0200

    Use submit event to add player

--- app/components/add-player.js
+++ app/components/add-player.js
@@ -1,9 +1,10 @@
-import Component from '@ember/component';
+import Component from '@ember/component'
 
 export default Component.extend({
   actions: {
-    add() {
-      this.get('add')(this.get('name'))
+    add(event) {
+      event.preventDefault()
+      this.get('onSubmit')(this.get('name'))
     }
   }
-});
+})
