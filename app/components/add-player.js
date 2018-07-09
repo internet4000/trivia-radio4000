f373f666b2cefe3dcaddb577461603bb0eb4db00
Oskar Rough <oskar@rough.dk>
2018-07-09 20:01:42 +0200

    Add default player names

--- app/components/add-player.js
+++ app/components/add-player.js
@@ -1,10 +1,15 @@
 import Component from '@ember/component'
+import {computed} from '@ember/object'
 
 export default Component.extend({
+  placeholder: computed('numberOfPlayers', function () {
+    return `Player ${this.numberOfPlayers + 1}`
+  }),
   actions: {
     add(event) {
       event.preventDefault()
-      this.get('onSubmit')(this.get('name'))
+      let name = this.get('name') || this.get('placeholder')
+      this.get('onSubmit')(name)
       this.set('name', '')
     }
   }
