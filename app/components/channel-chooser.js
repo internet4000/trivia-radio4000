7aaedb0674a8a6e60166b2674a379ff8b91682e6
Oskar Rough <oskar@rough.dk>
2018-07-14 19:53:40 +0200

    feat: only show channels with tracks

--- app/components/channel-chooser.js
+++ app/components/channel-chooser.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component'
+import {filterBy} from '@ember/object/computed'
 
 export default Component.extend({
+  channelsWithTracks: filterBy('channels', 'totalTracks'),
   actions: {
     select(channel) {
       this.onSelect(channel)
