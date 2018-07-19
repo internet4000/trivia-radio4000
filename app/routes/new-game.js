c110b9b945ce8961aa32f5d48224e5e2eff5be06
Oskar Rough <oskar@rough.dk>
2018-07-20 01:33:51 +0200

    Make it possible create quiz via URL

--- app/routes/new-game.js
+++ app/routes/new-game.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route'
+import {hash} from 'rsvp'
 
 function randomText(length = 4) {
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
@@ -10,7 +11,30 @@ function randomText(length = 4) {
 }
 
 export default Route.extend({
-  beforeModel() {
-    this.transitionTo('quiz', randomText())
+  async model({slug, questions}) {
+    const id = randomText()
+    const quiz = this.store.createRecord('quiz', {id})
+    const channels = this.store.findAll('channel')
+
+    if (slug) {
+      const query = await this.store.query('channel', {
+        orderBy: 'slug',
+        equalTo: slug
+      })
+      const channel = query.get('firstObject')
+      quiz.set('channel', channel)
+    }
+
+    if (slug && questions) {
+      quiz.set('totalQuestions', questions)
+    }
+
+    return hash({quiz, channels})
+  },
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      // isExiting would be false if only the route's model was changing
+      controller.set('slug', null)
+    }
   }
 })
--- app/routes/quiz.js
+++ app/routes/quiz.js
@@ -2,39 +2,28 @@ import Route from '@ember/routing/route'
 
 export default Route.extend({
   async model({id}) {
-    if (!id) this.transitionTo('new-game')
-
-    let store = this.store
-    let quiz = store.peekRecord('quiz', id)
-
-    if (!quiz) {
-      quiz = store.createRecord('quiz', {id})
+    if (!id) {
+      this.transitionTo('new-game')
     }
-
+    const quiz = this.store.peekRecord('quiz', id)
+    if (!quiz || !quiz.get('channel.id')) {
+      this.transitionTo('new-game')
+    }
+    return quiz
+  },
+  afterModel(quiz) {
     let players = quiz.get('players')
-
     if (!players.length) {
-      let player = store.createRecord('player', {
+      console.log('adding one player')
+      let player = this.store.createRecord('player', {
         name: 'Player 1'
       })
       players.pushObject(player)
     }
-
-    // hardcoded channel to speed up development
-    // let channel = await this.store.findRecord('channel', '-J_Mp1OhyLuo4tJozPAV')
-    // quiz.set('channel', channel)
-
-    return quiz
   },
   actions: {
     startQuiz() {
-      return this.currentModel.get('channel')
-        .then(channel => {
-          if(channel) {
-            return this.transitionTo('quiz.questions.question', 1)
-          }
-          return this.transitionTo('quiz.channels')
-        })
+      this.transitionTo('quiz.questions.question', 1)
     }
   }
 })
