9aeddda3ec1f87b6b1d46209df6c210428019bb2
Oskar Rough <oskar@rough.dk>
2019-12-27 11:52:21 +0100

    Upgrade ember

--- app/router.js
+++ app/router.js
@@ -1,10 +1,10 @@
 import EmberRouter from '@ember/routing/router'
 import config from './config/environment'
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-})
+export default class Router extends EmberRouter {
+  location = config.locationType
+  rootURL = config.rootURL
+}
 
 Router.map(function() {
   this.route('new-game')
@@ -16,7 +16,5 @@ Router.map(function() {
     })
     this.route('settings')
   })
-  this.route('about');
+  this.route('about')
 })
-
-export default Router
