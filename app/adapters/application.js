aad49f9bb06540efb0678e6a8c649c712a9ee42c
Oskar Rough <oskar@rough.dk>
2019-12-27 11:52:33 +0100

    Upgrade emberfire

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,7 @@
-import FirebaseAdapter from 'emberfire/adapters/firebase';
+import RealtimeDatabaseAdapter from 'emberfire/adapters/realtime-database';
 
-export default FirebaseAdapter.extend({
+export default RealtimeDatabaseAdapter.extend({
+    // Uncomment the following lines to enable offline persistence and multi-tab support
+    // enablePersistence: true,
+    // persistenceSettings: { synchronizeTabs: true }
 });
